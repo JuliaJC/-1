e6d66b833ed850c00b7e21863369ee7826337abe
Julia <julso@LAPTOP-J5CQF40T.(none)>
2022-11-13 19:03:15 +0300

    Zadacha4 added

---
+++ Zadacha4/Program.cs
@@ -0,0 +1,22 @@
+﻿Console.Write("Введите первое число: ");
+int numberOne = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите второе число: ");
+int numberTwo = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите третье число: ");
+int numberThree = Convert.ToInt32(Console.ReadLine());
+
+if(numberOne>numberTwo && numberOne>numberThree)
+{
+    Console.Write("Максимальное число: ");
+    Console.WriteLine(numberOne);
+}
+else if(numberTwo>numberThree)
+{
+    Console.Write("Максимальное число: ");
+    Console.WriteLine(numberTwo);
+}
+else
+{
+    Console.Write("Максимальное число: ");
+    Console.WriteLine(numberThree);
+}
