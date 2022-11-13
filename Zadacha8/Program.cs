bf24e4f067198115a654bbe1cb7db7698f38f68f
Julia <julso@LAPTOP-J5CQF40T.(none)>
2022-11-13 19:41:36 +0300

    Zadacha8 added

---
+++ Zadacha8/Program.cs
@@ -0,0 +1,13 @@
+﻿Console.Write("Введите число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+
+int numberOne = 2;
+
+while(numberOne <= number)
+{
+    Console.Write(numberOne + " ");
+    numberOne=numberOne+2;
+}
+
+ 
+ 
