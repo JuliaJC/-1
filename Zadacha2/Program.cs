0252bc1adf174f9eb9de9492ab3d85f683f57a2f
Julia <julso@LAPTOP-J5CQF40T.(none)>
2022-11-13 18:53:30 +0300

    Zadacha2 added

---
+++ Zadacha2/Program.cs
@@ -0,0 +1,15 @@
+﻿Console.Write("Введите первое число: ");
+int numberOne = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите второе число: ");
+int numberTwo = Convert.ToInt32(Console.ReadLine());
+
+if (numberOne > numberTwo)
+{
+    Console.Write("Большее число ");
+    Console.WriteLine(numberOne);
+}
+else
+{
+    Console.Write("Большее число ");
+    Console.WriteLine(numberTwo);
+}
